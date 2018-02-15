df28ae9c8ac25eba1681ceff0757d14f91ddb6d0
ceer006 <ceer006@gmail.com>
2018-02-15 19:52:06 +0300

    new header

--- source/js/menu-open.js
+++ source/js/menu-open.js
@@ -1,30 +1,11 @@
-var navMain = document.querySelector('.main-nav');
-var navList = document.querySelector('.site-list');
-var navToggle = document.querySelector('.main-nav__toggle');
-var navwrap = document.querySelector('.main-nav__logo');
-var app = document.querySelector('.app');
+var menuBtn = document.querySelector(".main-nav__toggler span");
+var menuBtnWrap = document.querySelector(".main-nav__toggler");
+var menu = document.querySelector(".main-nav__list");
 
-navMain.classList.remove('main-nav--nojs');
-app.classList.remove('app--nojs');
+menuBtnWrap.classList.toggle("main-nav__toggler--active");
+menu.classList.toggle("main-nav__list--visible");
 
-navToggle.addEventListener('click', function() {
-  if (navList.classList.contains('site-list--closed')) {
-    navList.classList.remove('site-list--closed');
-    navList.classList.add('site-list--opened');
-    navToggle.classList.remove('main-nav__toggle--opened');
-    navToggle.classList.add('main-nav__toggle--closed');
-    navwrap.classList.add('main-nav__logo--opened');
-    navwrap.classList.remove('main-nav__logo--closed');
-    app.classList.add('app--opened');
-    app.classList.remove('app--closed');
-  } else {
-    navList.classList.add('site-list--closed');
-    navList.classList.remove('site-list--opened');
-    navToggle.classList.remove('main-nav__toggle--closed');
-    navToggle.classList.add('main-nav__toggle--opened');
-    navwrap.classList.remove('main-nav__logo--opened');
-    navwrap.classList.add('main-nav__logo--closed');
-    app.classList.remove('app--opened');
-    app.classList.add('app--closed');
-  }
+menuBtn.addEventListener("click", function() {
+  menuBtnWrap.classList.toggle("main-nav__toggler--active");
+  menu.classList.toggle("main-nav__list--visible");
 });
